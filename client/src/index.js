7d9ba44fc13d82618ee66342573d0358b028d906
Eren Çifci <eren.cifci04@gmail.com>
2022-04-29 20:33:17 +0300

    Connect apollo

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
+import { ApolloProvider } from "@apollo/client";
+
+import client from "./apollo";
+import { BrowserRouter as Router } from "react-router-dom";
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <Router>
+            <App />
+        </Router>
+    </ApolloProvider>,
+    document.getElementById("root")
 );
